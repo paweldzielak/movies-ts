4f25e30eabce98cc9d4b6abae898ed8c794b071e
Paweł Dziełak <pawel.programowanie@hotmail.com>
2023-05-15 23:00:11 +0200

    handling favourite from numbers details

--- src/components/movie-details/NumberDetails.component.tsx
+++ src/components/movie-details/NumberDetails.component.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { MovieDetailsT } from "../../types/types";
+import { useUserDataContext } from "../../context/user-data.context";
 
 import "./numberDetails.styles.scss";
 
 const NumberDetails: React.FC<{ details: MovieDetailsT }> = ({ details }) => {
+  const { favoritesMovies, handleBookmarked } = useUserDataContext();
+
   return (
     <div className="media-details__numbers">
       <div className="media-details__numbers-rating">
@@ -12,7 +15,11 @@ const NumberDetails: React.FC<{ details: MovieDetailsT }> = ({ details }) => {
       </div>
       <div className="media-details__numbers-rating">
         <div className="rating-title">Your Favorite</div>
-        <img src="/favorite.svg" alt="" />
+        <img
+          src={favoritesMovies.includes(details.id) ? "/favorite.svg" : "notfavorite_light.svg"}
+          alt=""
+          onClick={() => handleBookmarked(details.id)}
+        />
       </div>
       <div className="media-details__numbers-rating">
         <div className="rating-title">popularity</div>
