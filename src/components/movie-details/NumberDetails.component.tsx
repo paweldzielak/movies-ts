e0674eeb1b1b72a770a6e1e3e8f9fd4a81f2289d
Paweł Dziełak <pawel.programowanie@hotmail.com>
2023-05-17 21:00:23 +0200

    more styles on details

--- src/components/movie-details/NumberDetails.component.tsx
+++ src/components/movie-details/NumberDetails.component.tsx
@@ -10,20 +10,26 @@ const NumberDetails: React.FC<{ details: MovieDetailsT }> = ({ details }) => {
   return (
     <div className="media-details__numbers">
       <div className="media-details__numbers-rating">
-        <div className="rating-title">TMDB Rating</div>
-        <div className="rating-child">{Math.round(details.vote_average * 100) / 100}</div>
+        <div className="rating-title">Rating</div>
+        <div className="rating-child">
+          <span className="rating-child__main">{Math.round(details.vote_average * 100) / 100}</span>
+          <span className="rating-child__secondary">/10</span>
+        </div>
       </div>
       <div className="media-details__numbers-rating">
-        <div className="rating-title">Your Favorite</div>
+        <div className="rating-title">Favorite</div>
         <img
           src={favoritesMovies.includes(details.id) ? "/favorite.svg" : "notfavorite_light.svg"}
           alt=""
           onClick={() => handleBookmarked(details.id)}
         />
       </div>
-      <div className="media-details__numbers-rating">
+      <div className="media-details__numbers-rating popularity-container">
         <div className="rating-title">popularity</div>
-        <div className="rating-child">{Math.round(details.popularity * 10) / 10}</div>
+        <div className="rating-child">
+          <img className="rating-child__icon" src="/popularity_chart.svg" alt="" />
+          <span className="rating-child__main">{Math.round(details.popularity * 10) / 10}</span>
+        </div>
       </div>
     </div>
   );
