7f6709b6900b8e91824bc519f220d479ad094498
Paweł Dziełak <pawel.programowanie@hotmail.com>
2023-07-12 08:16:22 +0200

    fixing tests unussed imports for netlify build

--- src/components/nav-bar/NavBar.test.tsx
+++ src/components/nav-bar/NavBar.test.tsx
@@ -1,24 +1,23 @@
+import { vi } from 'vitest';
+
 import { render, screen, waitFor } from '@testing-library/react';
 import user from '@testing-library/user-event';
 
-import { vi } from 'vitest';
-
 import NavBar from './NavBar.component';
 
-import { MovieContext, useMovieContext } from '../../context/movies.context';
+import { MovieContextProvider, MovieContextState } from '../../context/movies.context';
 
-const handleSearch = vi.fn()
-beforeEach(() => {
-  const values = useMovieContext();
+const renderComponentWithDefaultContext = (() => {
   render(
-    <MovieContext.Provider value={{...values,  handleSearch}}>
+    <MovieContextProvider >
       <NavBar />
-    </MovieContext.Provider>
+    </MovieContextProvider>
   )
-  screen.debug();
+  // screen.debug();
 })
 
 test('NavBar contains three buttons and text input', () => {
+  renderComponentWithDefaultContext();
   const input = screen.getByPlaceholderText(/enter title/i);
   const buttons = screen.getAllByRole('button');
 
@@ -26,22 +25,44 @@ test('NavBar contains three buttons and text input', () => {
   expect(buttons).toHaveLength(3);
 })
 
-// test('Changing "favorite" button text after click', async () => {
-//   const btnOld = screen.getByRole('button', { name: /show favorites only/i })
-//   expect(btnOld).toBeEnabled();
-//   user.click(btnOld);
 
-//   await waitFor(() => expect(screen.queryByRole('button', { name: /show favorites only/i })).not.toBeInTheDocument());
+test('Changing "favorite" button text after click', async () => {
+  renderComponentWithDefaultContext()
+  const btnOld = screen.getByRole('button', { name: /show favorites only/i })
+  expect(btnOld).toBeEnabled();
+  user.click(btnOld);
 
-//   const btnNew = screen.getByRole('button', { name: /show all results/i });
-//   expect(btnNew).toBeInTheDocument();
-// })
+  await waitFor(() => expect(screen.queryByRole('button', { name: /show favorites only/i })).not.toBeInTheDocument());
 
-// test('checks if function is called when the input value changes', () => {
-//   const emptyInput = screen.getByPlaceholderText(/enter title/i);
-//   expect(emptyInput).toBeEnabled();
-//   user.type(emptyInput, 'aaa');
-  
-//   expect(handleSearch).toHaveBeenCalledTimes(0);
+  const btnNew = screen.getByRole('button', { name: /show all results/i });
+  expect(btnNew).toBeInTheDocument();
+})
 
-// })
+test('checks if function is called when the input value changes', async () => {
+
+  const actual = await vi.importActual<typeof import("../../context/movies.context")>("../../context/movies.context")
+  const MovieContext = actual.MovieContext;
+  const handleSearch = vi.fn()
+
+  const mockedFunctions = {
+    handleSearch
+  }
+
+  const handleSearchSpy = vi.spyOn(mockedFunctions, 'handleSearch')
+  const contextValue = {} as MovieContextState;
+
+  render(
+    <MovieContext.Provider value={contextValue} >
+      <NavBar />
+    </MovieContext.Provider>
+  )
+
+  // const handleSearchSpy = vi.spyOn(spyMethods, 'handleSearch');
+  const emptyInput = screen.getByPlaceholderText(/enter title/i);
+  expect(emptyInput).toBeEnabled();
+  await user.type(emptyInput, 'aaa');
+
+  // screen.debug()
+  await waitFor(() => expect(handleSearchSpy).toHaveBeenCalled());
+
+})
